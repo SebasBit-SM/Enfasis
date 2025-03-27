059aa253677b9b6d894d4dbf812982253a5fcf18
SebasBit-SM <sebas2004lupe@gmail.com>
2025-03-26 22:34:24 -0500

    se ajusta app.js

--- src/app/app.js
+++ src/app/app.js
@@ -6,6 +6,7 @@ const config = require("../config");
 const app = express();
 
 const roles = require("../routes/rol.routes");
+const userRoutes = require("./routes/user.routes");
 
 //middlewares de configuracion
 app.use(cors());
@@ -23,5 +24,6 @@ app.set('port',config.app.port);
 
 //Rutas
 app.use("/api/rol", roles);
+app.use("/users", userRoutes);
 
 module.exports = app;
